8010aa0d93b9fabb1f36ecdf76a37e638792d456
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-20 15:23:31 +0100

    feat: secondName variable from user prompt

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@
 const firstName = prompt('First Name');
 
 console.log ('First Name:',firstName);
+
+
+// initialize lastName variable from second user prompt
+
+const lastName = prompt('Last Name');
+
+console.log ('Last Name:',lastName);
+
+
